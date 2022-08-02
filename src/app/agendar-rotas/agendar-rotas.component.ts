e88d7da18ea7f52b354a16aa6a79dd9490c00231
jonathanrossato <jonathan.e.rossato@gmail.com>
2022-08-02 20:32:31 -0300

    ajuste tela agendar + adicionar rotas

--- src/app/agendar-rotas/agendar-rotas.component.ts
+++ src/app/agendar-rotas/agendar-rotas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
 
 @Component({
   selector: 'app-agendar-rotas',
@@ -15,8 +15,17 @@ export class AgendarRotasComponent implements OnInit {
   input2: any;
   input3: any;
   input4: any;
-   
+  input5: any;
+  input6: any;
+  
+  parada2show: boolean = false;
+  parada3show: boolean = false;
+  parada4show: boolean = false;
   ngOnInit() {
+    this.parada2show = false;
+    this.parada3show = false;
+    this.parada4show= false;
+
     let loader = new Loader({
       apiKey: 'AIzaSyCbu9PxUAnPqy2W1fyKwLANXFywzDyiDKI',
       libraries: ['places']
@@ -52,16 +61,16 @@ export class AgendarRotasComponent implements OnInit {
         types: ['(cities)']
       }
 
-      this.input1 = document.getElementById("inicio");
-      if (this.input1 !== null) {
+      this.input1 = document.getElementById("partida");
+      if (this.input1 !== null) {        
         var autocomplete1 = new google.maps.places.Autocomplete(this.input1, options);
       }
 
-      this.input2 = document.getElementById("fim");
+      this.input2 = document.getElementById("destino");
       if (this.input2 !== null) {
         var autocomplete1 = new google.maps.places.Autocomplete(this.input2, options);
       }
-      this.input3 = document.getElementById("parada1");
+      this.input3 = document.getElementById("parada");
       if (this.input3 !== null) {
         var autocomplete1 = new google.maps.places.Autocomplete(this.input3, options);
       }
@@ -69,6 +78,14 @@ export class AgendarRotasComponent implements OnInit {
       this.input4 = document.getElementById("parada2");
       if (this.input4 !== null) {
         var autocomplete1 = new google.maps.places.Autocomplete(this.input4, options);
+      }
+      this.input5 = document.getElementById("parada3");
+      if (this.input5 !== null) {
+        var autocomplete1 = new google.maps.places.Autocomplete(this.input5, options);
+      }
+      this.input6 = document.getElementById("parada4");
+      if (this.input6 !== null) {
+        var autocomplete1 = new google.maps.places.Autocomplete(this.input6, options);
       }
       
     });
@@ -79,45 +96,80 @@ export class AgendarRotasComponent implements OnInit {
     directionsRenderer: google.maps.DirectionsRenderer
   ) {
 
+
     const waypts: google.maps.DirectionsWaypoint[] = [];
-    waypts.push({
-      location: (document.getElementById("parada1") as HTMLInputElement).value,
-      stopover: true,
-    });
-    waypts.push({
-      location: (document.getElementById("parada2") as HTMLInputElement).value,
-      stopover: true,
-    });
 
+    if( (document.getElementById("parada") as HTMLInputElement).value != ''){
+      waypts.push({
+        location: (document.getElementById("parada") as HTMLInputElement).value,
+        stopover: true,
+      });
+    }
+    if( (document.getElementById("parada2") as HTMLInputElement).value != ''){
+      waypts.push({
+        location: (document.getElementById("parada2") as HTMLInputElement).value,
+        stopover: true,
+      });
+    }
+  
+    if( (document.getElementById("parada3") as HTMLInputElement).value != ''){
+      waypts.push({
+        location: (document.getElementById("parada3") as HTMLInputElement).value,
+        stopover: true,
+      });
+    }
 
-    directionsService
+    if( (document.getElementById("parada4") as HTMLInputElement).value != ''){
+      waypts.push({
+        location: (document.getElementById("parada4") as HTMLInputElement).value,
+        stopover: true,
+      });
+    }
+    if( (document.getElementById("partida") as HTMLInputElement).value != '' && (document.getElementById("destino") as HTMLInputElement).value != ''){
+
+      directionsService
       .route({
         origin: {
-          query: (document.getElementById("inicio") as HTMLInputElement).value,
+          query: (document.getElementById("partida") as HTMLInputElement).value,
         },
         destination: {
-          query: (document.getElementById("fim") as HTMLInputElement).value,
+          query: (document.getElementById("destino") as HTMLInputElement).value,
         },
         waypoints: waypts,
         travelMode: google.maps.TravelMode.DRIVING,
+        optimizeWaypoints :true,
+        provideRouteAlternatives: true
       })
       .then((response) => {
         directionsRenderer.setDirections(response);
       })
     //.catch((e) => window.alert("Directions request failed due to " + status));
-  }
-
-
-  setMarkers(map: google.maps.Map) {
-
-
+    }
+    
 
 
 
   }
 
+ 
+  
+  
+  adicionarParadaTela(){   
+    if(this.parada2show === false){     
+      this.parada2show = true; 
+      return;     
+    }
+    
+    if(this.parada3show === false){     
+      this.parada3show = true; 
+      return;     
+    }
 
-
+    if(this.parada4show === false){     
+      this.parada4show = true; 
+      return;     
+    }
+  }
 
 
 
