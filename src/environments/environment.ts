a5ca04f5a50f7ff4801a2451647b1edefab13f11
Jonathan Rossato <jonathan.e.rossato@gmail.com>
2023-02-02 22:41:31 -0300

    fix: environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,   
-  apikey:'AIzaSyCbFrQ-caNKZesfJdUxvuhQatVwJhnSp6c'
+  production: false,
+  apikey:'AIzaSyCbFrQ-caNKZesfJdUxvuhQatVwJhnSp6c',
+  apikeyDev:'AIzaSyDYR45TvExNG498aRNe_C2MS4R0p4EKS4U',
+  client_id: '',
+  project_id: 'smart-geo-maps',
+  auth_uri: ' https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_secret: '',
+  access_token: '',
+  scope: 'https://www.googleapis.com/auth/cloud-billing.readonly',
+  token_type: 'Bearer',
+  refresh_token: '',  
 };
 
 /*
