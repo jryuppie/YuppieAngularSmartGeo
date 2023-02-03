14e977a5d342a51e3d4eb0769210c494c1165ebb
Jonathan Rossato <jonathan.e.rossato@gmail.com>
2023-02-02 22:44:49 -0300

    fix: enviroment cant read prod atribute fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apikey:'AIzaSyCbFrQ-caNKZesfJdUxvuhQatVwJhnSp6c',
   apikeyDev:'AIzaSyDYR45TvExNG498aRNe_C2MS4R0p4EKS4U',
   client_id: '',
